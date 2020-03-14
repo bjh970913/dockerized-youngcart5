d11c014c2d45f0f39727d071717e70f9efbae664
bjh970913 <bjh970913@gmail.com>
2020-03-14 17:37:13 +0900

    Add window support

--- restart.sh
+++ restart.sh
@@ -7,4 +7,10 @@
 echo "sleep 5 sec for database loading..."
 sleep 5
 
-open "http://localhost:8080/"
+command -v open
+if [[ $? -eq 0 ]]; then
+    open "http://localhost:8080/"
+else
+    start "http://localhost:8080/"
+fi
+
